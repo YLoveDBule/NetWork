1d09feef630e487bfc90c5d09443ceb54a29ce0a
DESKTOP-KNABA4F\yangjunjie <yangjunjie@example.com>
2017-07-25 18:35:48 +0800

    ddsfsffs

--- GameNetWorkManager.cpp
+++ GameNetWorkManager.cpp
@@ -1,4 +1,6 @@
 #include "GameNetWorkManager.h"
+#include <assert.h>
+
 
 GameNetWorkManager::~GameNetWorkManager()
 {
@@ -10,4 +12,261 @@ GameNetWorkManager& GameNetWorkManager::getInstance()
 {
 	static GameNetWorkManager s_GameNetWorkManager;
 	return s_GameNetWorkManager;
+}
+
+void GameNetWorkManager::start()
+{
+	if (!m_isInited)
+	{
+		m_isInited = m_isValid = true;
+	}
+}
+
+void GameNetWorkManager::clear()
+{
+	for (auto itor = m_gameNetWorkPool.begin(); itor != m_gameNetWorkPool.end(); ++itor)
+	{
+		if (itor->second)
+		{
+			itor->second->closeSock();
+			itor->second.reset(nullptr);
+		}
+	}
+
+	m_recorderSID.clear();
+	m_gameNetWorkPool.clear();
+	while (!m_gameNetWorkStack.empty())
+	{
+		if (m_gameNetWorkStack.top())
+		{
+			m_gameNetWorkStack.top().reset(nullptr);
+			m_gameNetWorkStack.pop();
+		}
+	}
+
+}
+
+void GameNetWorkManager::end()
+{
+	m_isValid = false;
+}
+
+void GameNetWorkManager::registerMsgAdopter(std::unique_ptr<NetMsgAdpoter> &adopter)
+{
+	m_msgAdopter.reset(adopter.release());
+}
+
+void GameNetWorkManager::askToSubserver(const int recorder, const char*ip, const int port)
+{
+	std::unique_ptr<GameNetWork> network = nullptr;
+	if (strlen(ip) != 0 && getValidNetwork(network))
+	{
+		if (network && network->init() && network->connectServer(ip,port))
+		{
+			addNewNet(network);
+			m_recorderSID.insert(std::make_pair(recorder, network->getSocket()));
+			
+		}
+	}
+	else
+	{
+		assert("failed to create the socket!!!");
+	}
+}
+
+void GameNetWorkManager::askToSubserverResult()
+{
+	for (auto itor : m_recorderSID)
+	{
+		pushServerAskResult(itor.first, itor.second);
+	}
+	m_recorderSID.clear();
+}
+
+void GameNetWorkManager::pushServerAskResult(const int recorder, const SOCKET socketId)
+{
+	if (m_msgAdopter)
+	{
+		m_msgAdopter->pushServerAskResult(recorder, socketId);
+	}
+}
+
+void GameNetWorkManager::addNewNet(std::unique_ptr<GameNetWork>& value)
+{
+	if (m_gameNetWorkPool.find(value->getSocket())== m_gameNetWorkPool.end())
+	{
+		m_gameNetWorkPool.insert(std::make_pair(value->getSocket(), value));
+	}
+}
+
+
+void GameNetWorkManager::removeNet(const SOCKET value)
+{
+	m_gameNetWorkPool.erase(value);
+}
+
+void GameNetWorkManager::collectIdleNW()
+{
+	if (m_gameNetWorkPool.size() > 0)
+	{
+		auto itor = m_gameNetWorkPool.begin();
+		while (itor != m_gameNetWorkPool.end())
+		{
+			if (itor->second && !itor->second->isActive())
+			{
+				itor->second.reset(nullptr);
+				itor = m_gameNetWorkPool.erase(itor);
+			}
+			else 
+			{
+				itor++;
+			}
+		}
+	}
+}
+
+bool GameNetWorkManager::getValidNetwork(std::unique_ptr<GameNetWork>& value)
+{
+	value = make_unique<GameNetWork>();
+	return (value != nullptr);
+}
+
+bool GameNetWorkManager::getIdleNetWork(std::unique_ptr<GameNetWork>& value)
+{
+	auto result = false;
+	if (m_gameNetWorkStack.size() != 0)
+	{
+		value.reset(m_gameNetWorkStack.top().release());
+		m_gameNetWorkStack.pop();
+		result = true;
+	}
+	return result;
+}
+
+void GameNetWorkManager::closeConnect(const int sid)
+{
+	if (m_gameNetWorkPool.find(sid) != m_gameNetWorkPool.end)
+	{
+		if (m_gameNetWorkPool.at(sid))
+		{
+			m_gameNetWorkPool.at(sid)->closeSock();
+			m_gameNetWorkStack.push(m_gameNetWorkPool.at(sid));
+		}
+		m_gameNetWorkPool.erase(sid);
+	}
+}
+
+void GameNetWorkManager::closeAllConnect()
+{
+	while (!m_gameNetWorkPool.empty())
+	{
+		auto itor = m_gameNetWorkPool.begin();
+		if (itor != m_gameNetWorkPool.end() && itor->second)
+		{
+			if (m_gameNetWorkPool.at(itor->first))
+			{
+				m_gameNetWorkPool.at(itor->first)->closeSock();
+			}
+			m_gameNetWorkPool.erase(itor);
+		}
+	}
+	m_gameNetWorkPool.clear();
+}
+
+void GameNetWorkManager::beginSendMsg(const SOCKET socketId)
+{
+	if (m_gameNetWorkPool.at(socketId))
+	{
+		m_gameNetWorkPool.at(socketId)->beginSendMsg();
+	}
+}
+
+void GameNetWorkManager::pushNum(const SOCKET socketId, void* data, const unsigned int size)
+{
+	if (m_gameNetWorkPool.at(socketId))
+	{
+		m_gameNetWorkPool.at(socketId)->pushNum(data, size);
+	}
+}
+
+void GameNetWorkManager::pushLString(const SOCKET socketId, const char* data, const int size)
+{
+	if (m_gameNetWorkPool.at(socketId))
+	{
+		m_gameNetWorkPool.at(socketId)->pushLString(data, size);
+	}
+}
+
+void GameNetWorkManager::pushLstringShortSize(const SOCKET socketId, const char* data, const short size)
+{
+	if (m_gameNetWorkPool.at(socketId))
+	{
+		m_gameNetWorkPool.at(socketId)->pushLString(data, size);
+	}
+}
+
+void GameNetWorkManager::endSendMsg(const SOCKET socketId)
+{
+	if (m_gameNetWorkPool.at(socketId))
+	{
+		m_gameNetWorkPool.at(socketId)->endSendMsg();
+	}
+}
+
+bool GameNetWorkManager::sendMessage(const SOCKET socketId, unsigned short wMainCmdID, unsigned short wSubCmdID, unsigned int& sendSize)
+{
+	auto result = false;
+	if (m_gameNetWorkPool.at(socketId))
+	{
+		result = m_gameNetWorkPool.at(socketId)->sendData(wMainCmdID, wSubCmdID, sendSize);
+	}
+	return result;
+}
+
+bool GameNetWorkManager::sendMessage(const SOCKET socketId, unsigned short wMainCmdID, unsigned short wSubCmdID, void *pData, unsigned int& sendSize)
+{
+	auto result = false;
+	if (m_gameNetWorkPool.at(socketId))
+	{
+		result = m_gameNetWorkPool.at(socketId)->sendData(wMainCmdID, wSubCmdID, pData, sendSize);
+	}
+	return result;
+}
+
+bool GameNetWorkManager::houseNetMessage(unsigned short wSocketID, TCP_Command command, void *pData, unsigned short wDataSize)
+{
+	if (m_msgAdopter)
+	{
+		return m_msgAdopter->houseNetMsg(wSocketID, command.wMainCmdID, command.wSubCmdID, pData,wDataSize);
+	}
+	return true;
+}
+
+bool GameNetWorkManager::OnEventTCPSocketShut(unsigned short wSocketID, unsigned char cbShutReason)
+{
+	if (m_msgAdopter)
+	{
+		return m_msgAdopter->onConnectShutdown(wSocketID, cbShutReason);
+	}
+	return true;
+}
+
+GameNetWorkManager::GameNetWorkManager()
+	:m_isValid(true),
+	m_isInited(false),
+	m_macAddress(false),
+	m_msgAdopter(nullptr)
+{
+	clear();
+}
+
+void GameNetWorkManager::updateNetWorkPool()
+{
+	for (auto itor = m_gameNetWorkPool.begin(); itor != m_gameNetWorkPool.end(); ++ itor)
+	{
+		if (itor->second)
+		{
+			itor->second->update();
+		}
+	}
 }
--- GameNetWorkManager.h
+++ GameNetWorkManager.h
@@ -3,13 +3,60 @@
 #define __GAMENETWORKMANAGER__
 #include "TCPSocket.h"
 #include "Define.h"
+#include "GameNetWork.h"
+#include <map>
+#include <stack>
+
+class NetMsgAdpoter
+{
+public:
+	virtual bool houseNetMsg(SOCKET wSocketID, unsigned short maincmd, unsigned short subcmd, void *pData, unsigned short wDataSize) = 0;
+	virtual bool houseProcMsg(SOCKET wSocketID, unsigned short maincmd, unsigned short subcmd, void *pData, unsigned short wDataSize) = 0;
+	virtual bool pushServerAskResult(const int recorder, SOCKET sid) = 0;
+	virtual bool onConnectShutdown(unsigned short sockid, unsigned char shutReason) = 0;
+};
+
 class GameNetWorkManager
 {
 public:
 	virtual ~GameNetWorkManager();
 	static GameNetWorkManager&getInstance();
+	virtual void start();
+	virtual void clear();
+	virtual void end();
+	virtual void registerMsgAdopter(std::unique_ptr<NetMsgAdpoter> &adopter);
+	virtual void askToSubserver(const int recorder, const char*ip, const int port);
+	virtual void askToSubserverResult();
+	virtual void pushServerAskResult(const int recorder, const SOCKET socketId);
+	virtual void addNewNet(std::unique_ptr<GameNetWork>&value);
+	virtual void removeNet(const SOCKET value);
+	virtual void collectIdleNW();
+	virtual bool getValidNetwork(std::unique_ptr<GameNetWork>& value);
+	virtual bool getIdleNetWork(std::unique_ptr<GameNetWork>& value);
+	virtual void closeConnect(const int sid);
+	virtual void closeAllConnect();
+	virtual void beginSendMsg(const SOCKET socketId);
+	virtual void pushNum(const SOCKET socketId, void* data, const unsigned int size);
+	virtual void pushLString(const SOCKET socketId, const char* data, const int size);
+	virtual void pushLstringShortSize(const SOCKET socketId, const char* data, const short size);
+	virtual void endSendMsg(const SOCKET socketId);
+	virtual bool sendMessage(const SOCKET socketId, unsigned short wMainCmdID,  unsigned short wSubCmdID,  unsigned int& sendSize);
+	virtual bool sendMessage(const SOCKET socketId, unsigned short wMainCmdID, unsigned short wSubCmdID, void *pData,unsigned int& sendSize);
+	virtual bool houseNetMessage(unsigned short wSocketID, TCP_Command command, void *pData, unsigned short wDataSize);
+	virtual bool OnEventTCPSocketShut(unsigned short wSocketID, unsigned char cbShutReason);
 private:
 	GameNetWorkManager();
+	void updateNetWorkPool();
+private:
+	bool m_isValid;
+	bool m_isInited;
+	char * m_macAddress;
+	std::unique_ptr<NetMsgAdpoter> m_msgAdopter;
+	typedef std::map<SOCKET, std::unique_ptr<GameNetWork>> GameNetMap;
+	GameNetMap m_gameNetWorkPool;
+	typedef std::stack<std::unique_ptr<GameNetWork>> GameNetWorkStack;
+	GameNetWorkStack m_gameNetWorkStack;
+	std::map<int, int > m_recorderSID;
 };
 
 #endif
